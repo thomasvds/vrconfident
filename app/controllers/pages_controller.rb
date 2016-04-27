08f5b627d6192490545f8cd1a2116efcd659418d
Thomas Vanderstraeten <vanderstraeten.thomas@gmail.com>
2016-04-27 16:27:16 +0200

    Website finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,30 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class PagesController < ApplicationController
   def home
+    @lead = Lead.new
+  end
+
+  def register
+    @lead = Lead.create(lead_params)
+    respond_to do |format|
+      if @lead.save
+        payload = {
+          username:         "New lead!",
+          text:             @lead.name + " " + @lead.email + " " + @lead.business.to_s
+        }
+        uri = URI.parse("https://hooks.slack.com/services/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")
+        Net::HTTP.post_form(uri, :payload => JSON.generate(payload))
+        format.html { redirect_to root_path, notice: "Thanks! We'll get back to you soon." }
+      else
+        format.html { redirect_to root_path, alert: "Not registered. Have you entered a correct email?"}
+      end
+    end
+  end
+
+  def lead_params
+    params.require(:lead).permit(:name, :email, :business)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: 'pages#home'
+
+  put "pages/register", to: "pages#register", as: "register"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
